5b6d9aa64422580f90eb18015e58df105fe2e99d
Arpit Jain <121026251+craj26@users.noreply.github.com>
2022-12-26 16:19:16 +0530

    Add files via upload

---
+++ src/CodeFiles/Compress.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CodeFiles;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ *
+ * @author arpit jain
+ */
+public class Compress {
+    public static void compressFunction(File file) throws FileNotFoundException, IOException{
+        String fileDirectory = file.getParent();
+        System.out.println(fileDirectory);
+        FileInputStream fis = new FileInputStream(file);
+        FileOutputStream fos = new FileOutputStream(fileDirectory + "/CompressedFile.gz");
+        GZIPOutputStream gzipOS = new GZIPOutputStream(fos);
+        
+        byte[] buffer = new byte[1024];
+        int len;
+        
+        while((len=fis.read(buffer))!= -1){
+            gzipOS.write(buffer,0, len);
+            //System.out.println(len);
+        }
+        
+        gzipOS.close();
+        fos.close();
+        fis.close();
+    }
+    public static void main(String []args) throws IOException{
+         File path = new File("C:\\Users\\arpit jain\\Documents\\NetBeansProjects\\accio-compressdecompress\\src\\CodeFiles\\testfile.txt");
+         
+         compressFunction(path);
+        
+    }
+    
+}
